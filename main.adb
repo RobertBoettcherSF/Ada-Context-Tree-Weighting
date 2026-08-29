33f18331d52189be51642699546e3340d83e6889
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-29 14:12:50 +0200

    Add main.adb with entry point and messages

---
+++ main.adb
@@ -0,0 +1,9 @@
+-- main.adb
+-- Stub entry point for the standard compilation flow.
+with Ada.Text_IO;
+
+procedure Main is
+begin
+   Ada.Text_IO.Put_Line ("Context Tree Weighting Library Loaded.");
+   Ada.Text_IO.Put_Line ("Execute 'make test' to run the V&V test suite.");
+end Main;
